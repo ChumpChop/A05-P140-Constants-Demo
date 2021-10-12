56c05b7f91644f774b459699a4a93d5fb938ab11
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-12 22:34:34 +0100

    Add project files.

---
+++ Constants Demo/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    'File name constant
+    Private Const strFileName As String = "C:\Temp\Hello.txt"
+
+    Private Sub btnOne_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnOne.Click
+
+        'Using a constant
+        MessageBox.Show("1: " & strFileName, "Constants Demo")
+
+    End Sub
+
+    Private Sub btnTwo_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnTwo.Click
+
+        'Using the constant again
+        MessageBox.Show("2: " & strFileName, "Constants Demo")
+
+    End Sub
+
+    Private Sub btnThree_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnThree.Click
+
+        'Reusing the constant one more time
+        MessageBox.Show("3: " & strFileName, "Constants Demo")
+
+    End Sub
+End Class
